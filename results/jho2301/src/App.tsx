71b9c23c23f7cc4700a15460a20317c388884ef6
jho2301 <jho2301@gmail.com>
2021-07-07 09:50:07 +0900

    chore: TS React Webpack 세팅

---
+++ results/jho2301/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1>HelloWorld ㅎ</h1>;
+};
+
+export default App;
---
+++ results/jho2301/src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const $root = document.getElementById('root');
+
+ReactDOM.render(<App />, $root);

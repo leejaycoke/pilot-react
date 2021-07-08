21810fec77f9abcb3019793e7b48e6a5ec466086
jho2301 <jho2301@gmail.com>
2021-07-08 23:30:21 +0900

    feat: 로그인한 사용자는 회원정보를 확인할 수 있다

--- results/jho2301/src/__test__/user.test.tsx
+++ results/jho2301/src/__test__/user.test.tsx
@@ -3,7 +3,7 @@ import { screen } from '@testing-library/react';
 import UserPage from '../pages/User/UserPage';
 import { myRender } from './testUtil';
 
-describe('LOGIN', () => {
+describe('USER', () => {
   test('로그인한 사용자는 회원정보를 확인할 수 있다.', async () => {
     myRender(<UserPage />);
 

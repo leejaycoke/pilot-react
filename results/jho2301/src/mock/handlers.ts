efee7fa441c4efc46810550fdc3a3743f77acc04
jho2301 <jho2301@gmail.com>
2021-07-08 22:42:01 +0900

    test: 회원이 아닐시, 에러를 표시한다 목서버 로직 수정

--- results/jho2301/src/mock/handlers.ts
+++ results/jho2301/src/mock/handlers.ts
@@ -3,9 +3,9 @@ import { rest } from 'msw';
 
 export const loginHandlers = [
   rest.post<LoginForm>('http://localhost:5000/auth/login', (req, res, ctx) => {
-    const { account } = req.body;
+    const { account, password } = req.body;
 
-    if (account === 'wrong') {
+    if (account !== 'devbadak' || password !== '1234') {
       return res(ctx.status(400), ctx.json({ message: 'wrong!' }));
     }
 

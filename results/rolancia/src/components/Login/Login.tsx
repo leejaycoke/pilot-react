7c4a12051e677140624cd104d04c8da98dfd6c83
jangjunhyeok <95jjangjun@gmail.com>
2021-07-08 20:58:59 +0900

    Add handleLogin into Login Component

--- results/rolancia/src/components/Login/Login.tsx
+++ results/rolancia/src/components/Login/Login.tsx
@@ -1,20 +1,46 @@
-import React from "react";
-import { HttpClientProps } from '../../model/interfaces';
+import React, { FormEvent, useRef } from "react";
+import { LoginEntity } from "../../model/entity";
+import { HttpClientProps } from "../../model/interfaces";
 import styles from "./Login.module.css";
 
-const Login: React.FC<HttpClientProps> = ({ httpClient }: HttpClientProps)=> {
+const Login: React.FC<HttpClientProps> = ({ httpClient }: HttpClientProps) => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const inputIdRef = useRef<HTMLInputElement>(null);
+  const inputPwdRef = useRef<HTMLInputElement>(null);
+
+  const handleLogin = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await httpClient.login(new LoginEntity(inputIdRef.current!.value, inputPwdRef.current!.value));
+    } catch (error) {
+      alert("로그인에 실패했습니다~! 메롱😋");
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleLogin} ref={formRef}>
       <div className={styles.form__row}>
-        <input className={styles.input__id} type="text" placeholder="아이디" />
+        <input
+          className={styles.input__id}
+          type="text"
+          placeholder="아이디"
+          ref={inputIdRef}
+        />
       </div>
       <div className={styles.form__row}>
-        <input className={styles.input__pwd} type="password" placeholder="비밀번호" />
+        <input
+          className={styles.input__pwd}
+          type="password"
+          placeholder="비밀번호"
+          ref={inputPwdRef}
+        />
       </div>
       <div className={`${styles.form__row} ${styles.two}`}>
         <div className={styles.save__id}>
           <input id="save__id__check" type="checkbox" />
-          <label className={styles.save__id__label}htmlFor="savdId">아이디 저장</label>
+          <label className={styles.save__id__label} htmlFor="savdId">
+            아이디 저장
+          </label>
         </div>
         <ul className={styles.menus}>
           <li className={styles.menu}>

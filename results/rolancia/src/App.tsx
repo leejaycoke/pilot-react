b04b15c99a2d7fefbdce9783f53996b3be66f57c
jangjunhyeok <95jjangjun@gmail.com>
2021-07-09 22:31:23 +0900

    Install react-router & Set basic routing

--- results/rolancia/src/App.tsx
+++ results/rolancia/src/App.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import styles from "./App.module.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
-import { HttpClientProps } from './model/interfaces';
+import UserInfo from "./components/UserInfo/UserInfo";
+import { HttpClientProps } from "./model/interfaces";
 
-const App: React.FC<HttpClientProps> = ({httpClient}: HttpClientProps) => {
+const App: React.FC<HttpClientProps> = ({ httpClient }: HttpClientProps) => {
   return (
-    <div className={styles.container}>
-      <Login httpClient={httpClient}/>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <div className={styles.container}>
+            <Login httpClient={httpClient} />
+          </div>
+        </Route>
+        <Route exact path="/user">
+        <div className={styles.container}> 
+          <UserInfo httpClient={httpClient}/>
+        </div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 

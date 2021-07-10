4f830884c82f4d532c19ce0c15b841d0fedd00fe
Kim-Jaemin420 <jany240@naver.com>
2021-07-11 00:47:23 +0900

    feat: post 요청과 get 요청에 대한 멤버 함수 선언

--- results/Kim-Jaemin420/pilot-react/src/FetchData/HttpFetch.ts
+++ results/Kim-Jaemin420/pilot-react/src/FetchData/HttpFetch.ts
@@ -4,21 +4,35 @@ class HttpFetch {
   private readonly url: string;
   private readonly data: any;
 
-  constructor(url: string, data: any) {
+  constructor(url: string, data: any = null) {
     this.url = url;
     this.data = data;
   }
 
-  async PostFetch () {
-    try {
-      const res = await axios.post(this.url, this.data);
-      return res.data;
+   PostFetch =  async () => {
+     try {
+       const res = await axios.post(this.url, this.data);
+       return res.data;
 
+     } catch (e) {
+       console.error(e);
+       return e.response.data;
+     }
+   };
+
+  GetFetch = async () => {
+    try {
+      const res = await axios.get(this.url,{
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+        },
+      });
+      return res;
     } catch (e) {
       console.error(e);
-      return e.response.data;
+      return e;
     }
-  }
+  };
 }
 
 export default HttpFetch;

9fcee9d095e08ab7a2c3bd88913c2b2029849262
Kim-Jaemin420 <jany240@naver.com>
2021-07-11 16:54:43 +0900

    import global style

--- results/Kim-Jaemin420/pilot-react/src/index.tsx
+++ results/Kim-Jaemin420/pilot-react/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
+import GlobalStyle from './styles/GlobalStyle';
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')

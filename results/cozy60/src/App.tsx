e0aee5eb95a04f9040528b34e182ee9f2a28190f
cozy60 <s2018w39@e-mirim.hs.kr>
2021-07-12 14:48:35 +0900

    feat: 라우팅 기능 추가

--- results/cozy60/src/App.tsx
+++ results/cozy60/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import UserInfoPage from './pages/UserInfoPage';
 
 function App() {
   return (
     <div>
-      <LoginPage />
+      <Route path="/" exact={true} component={LoginPage} />
+      {/* FIXME :: 인증된 유저만 접속 가능하게 수정 */}
+      <Route path="/users" component={UserInfoPage} />
     </div>
   );
 }
--- results/cozy60/src/index.tsx
+++ results/cozy60/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App'; 
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-); 
+);

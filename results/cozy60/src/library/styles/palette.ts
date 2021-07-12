146d893fd5f0bad6b0c655229bfb61c6310b1399
cozy60 <s2018w39@e-mirim.hs.kr>
2021-07-12 16:13:00 +0900

    refactor: class로 변경

--- results/cozy60/src/library/styles/palette.ts
+++ results/cozy60/src/library/styles/palette.ts
@@ -1,11 +1,23 @@
-const palette = {
-  black: '#000',
-  dark: '#191a20',
-  input: '#f3f3f4',
-  border: '#e0e2e7',
-  red: '#f86d7d',
-  blue: '#3da5f5',
-  white: '#fff',
-};
+class Palette {
+  black: string;
+  dark: string;
+  input: string;
+  border: string;
+  red: string;
+  blue: string;
+  white: string;
+
+  constructor() {
+    this.black = '#000';
+    this.dark = '#191a20';
+    this.input = '#f3f3f4';
+    this.border = '#e0e2e7';
+    this.red = '#f86d7d';
+    this.blue = '#3da5f5';
+    this.white = '#fff';
+  }
+}
+
+const palette = new Palette();
 
 export default palette;

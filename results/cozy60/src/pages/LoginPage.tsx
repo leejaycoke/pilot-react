5b6ac818535766e2566d849ea28170214e71f730
cozy60 <s2018w39@e-mirim.hs.kr>
2021-07-12 17:51:44 +0900

    feat: 로그인&로그아웃 기능 추가

--- results/cozy60/src/pages/LoginPage.tsx
+++ results/cozy60/src/pages/LoginPage.tsx
@@ -1,27 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../components/Card';
-import LoginForm from '../components/LoginForm';
+import Form from '../components/Form';
 import Input from '../components/Input';
 import Footer from '../components/Footer';
 import Button from '../components/Button';
 import ErrorMessage from '../components/ErrorMessage';
+import { signIn } from '../library/api/auth/auth';
+import { useHistory } from 'react-router-dom';
 
 function LoginPage() {
+  const [input, setInput] = useState({
+    account: '',
+    password: '',
+  });
+  const { account, password } = input;
+  const [error, setError] = useState('');
+  const history = useHistory();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await signIn(account, password);
+      history.push('/users');
+    } catch (e) {
+      setError(e);
+      console.log(e);
+    }
+  };
+
   return (
     <Card>
-      <LoginForm>
+      <Form onSubmit={onSubmit}>
         <label htmlFor="account">Username</label>
-        <Input type="text" name="account" id="account" />
+        <Input type="text" name="account" id="account" onChange={onChange} />
         <label htmlFor="password">Password</label>
-        <Input type="password" name="password" id="password" />
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          onChange={onChange}
+        />
         <Footer>
           <Button type="submit" color="blue">
             login
           </Button>
         </Footer>
         {/* FIXME :: error가 true일시 메세지 노출하도록 수정 */}
-        <ErrorMessage>1~4글자만 입력할 수 있습니다.</ErrorMessage>
-      </LoginForm>
+        {/* {error && <ErrorMessage>{error}</ErrorMessage>} */}
+      </Form>
     </Card>
   );
 }
--- results/cozy60/src/pages/UserInfoPage.tsx
+++ results/cozy60/src/pages/UserInfoPage.tsx
@@ -3,21 +3,38 @@ import Card from '../components/Card';
 import UserInfo from '../components/UserInfo';
 import Footer from '../components/Footer';
 import Button from '../components/Button';
+import Form from '../components/Form';
+import { signOut } from '../library/api/auth/auth';
+import { useHistory } from 'react-router-dom';
 
 function UserInfoPage() {
+  const history = useHistory();
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      signOut();
+      history.push('/');
+    } catch (e) {
+      // FIXME :: error message로 변경
+      console.log(e);
+    }
+  };
+
   return (
     <Card>
-      <div style={{ width: 412 }}>
-        <h2>🐾 회원 정보</h2>
-        {/* FIXME :: 동적으로 정보 입력되도록 수정 */}
-        <UserInfo dt="account" dd="devbadak" />
-        <UserInfo dt="name" dd="개발바닥" />
-        <UserInfo dt="level" dd="1" />
-        <Footer>
-          <Button type="button" color="red">
-            logout
-          </Button>
-        </Footer>
+      <div style={{ width: '400px' }}>
+        <Form onSubmit={onSubmit}>
+          <h2>🐾 회원 정보</h2>
+          {/* FIXME :: 동적으로 정보 입력되도록 수정 */}
+          <UserInfo dt="account" dd="devbadak" />
+          <UserInfo dt="name" dd="개발바닥" />
+          <UserInfo dt="level" dd="1" />
+          <Footer>
+            <Button type="submit" color="red">
+              logout
+            </Button>
+          </Footer>
+        </Form>
       </div>
     </Card>
   );

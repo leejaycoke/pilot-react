27180abd158f61a256dc38081d7a3e3968ea98a7
Leehaeun0 <a01050675111@gmail.com>
2021-07-12 12:44:11 +0900

    feat: Api class 에 login, getUser 메소드 제작

---
+++ results/leehaeun0/src/apis/api.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosInstance } from 'axios';
+
+import { AsyncFc } from '../hooks/useAsync';
+import Auth from '../utils/auth';
+
+export type AuthResponse = {
+  accessToken: string;
+};
+
+export type User = {
+  id: number;
+  account: string;
+  name: string;
+  level: number;
+};
+
+class Api {
+  private readonly apiClient: AxiosInstance;
+
+  constructor() {
+    this.apiClient = axios.create({
+      baseURL: process.env.REACT_APP_BASEURL,
+      timeout: 1000,
+    });
+  }
+
+  public login: AsyncFc<AuthResponse> = async ([payload]) => {
+    const response = await this.apiClient.post('/auth/login', payload);
+
+    console.log('login response: ', response);
+    Auth.set(response.data.accessToken);
+    return response.data;
+  };
+
+  public getUser: AsyncFc<User> = async (_, config): Promise<User> => {
+    const response = await this.apiClient.get('/v1/users/me', config);
+
+    console.log('getUser response: ', response);
+    return response.data;
+  };
+}
+
+export default new Api();

1727c949dab6756a730df52b3bb7b4f17fbfb5ea
Leehaeun0 <a01050675111@gmail.com>
2021-07-12 12:30:41 +0900

    feat: LoadingPage 업데이트

--- results/leehaeun0/src/pages/LoadingPage.tsx
+++ results/leehaeun0/src/pages/LoadingPage.tsx
@@ -3,15 +3,29 @@ import React from 'react';
 import styled from 'styled-components';
 
 const LoadingPage: React.FC = () => {
-  return <StyledCircularProgress />;
+  return (
+    <StyledMain>
+      <StyledWrapper>
+        <CircularProgress size={46} thickness={4.3} />
+      </StyledWrapper>
+    </StyledMain>
+  );
 };
 
-const StyledCircularProgress = styled(CircularProgress)`
+const StyledMain = styled.main`
+  min-height: 100vh;
+  background-color: ${({ theme }) => theme.color.lightGray};
+`;
+
+const StyledWrapper = styled.div`
   position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   z-index: 100;
+  svg circle {
+    color: ${({ theme }) => theme.color.main};
+  }
 `;
 
 export default LoadingPage;

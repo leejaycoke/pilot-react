1de9869b23eb512cdfd83621ce99b203d649af09
Leehaeun0 <a01050675111@gmail.com>
2021-07-12 12:32:05 +0900

    feat: App.tsx 에 Providers 추가

--- results/leehaeun0/src/App.tsx
+++ results/leehaeun0/src/App.tsx
@@ -1,18 +1,25 @@
 import React, { Suspense } from 'react';
+import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import LoadingPage from './pages/LoadingPage';
 import MainRouter from './routes/MainRouter';
 import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <GlobalStyle />
-        <Suspense fallback={<LoadingPage />}>
-          <MainRouter />
-        </Suspense>
+        <HelmetProvider>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Suspense fallback={<LoadingPage />}>
+              <MainRouter />
+            </Suspense>
+          </ThemeProvider>
+        </HelmetProvider>
       </BrowserRouter>
     </div>
   );

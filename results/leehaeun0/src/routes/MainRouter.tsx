38e2c8aea71878759505660f6d58c419c738ae34
Leehaeun0 <a01050675111@gmail.com>
2021-07-12 21:48:38 +0900

    Fix: PublicRoute PrivateRoute 컴포넌트 분리

--- results/leehaeun0/src/routes/MainRouter.tsx
+++ results/leehaeun0/src/routes/MainRouter.tsx
@@ -1,9 +1,10 @@
 import { lazy } from '@loadable/component';
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Switch } from 'react-router-dom';
 
 import Auth from '../utils/auth';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 const LoginPage = lazy(() => import('../pages/LoginPage'));
 const UserPage = lazy(() => import('../pages/UserPage'));
@@ -13,10 +14,10 @@ function MainRouter() {
 
   return (
     <Switch>
-      <Route exact path="/login">
+      <PublicRoute exact path="/login" redirect="/user">
         <LoginPage />
-      </Route>
-      <PrivateRoute exact path="/user">
+      </PublicRoute>
+      <PrivateRoute exact path="/user" redirect="/login">
         <UserPage />
       </PrivateRoute>
       {accessToken ? <Redirect to="/user" /> : <Redirect to="/login" />}
--- results/leehaeun0/src/routes/PublicRoute.tsx
+++ results/leehaeun0/src/routes/PublicRoute.tsx
@@ -3,20 +3,20 @@ import { Redirect, Route, RouteProps } from 'react-router-dom';
 
 import Auth from '../utils/auth';
 
-interface PrivateRouteProps extends RouteProps {
+interface PublicRouteProps extends RouteProps {
   children: React.ReactChild;
   path: string;
   redirect: string;
 }
 
-function PrivateRoute({ children, path, redirect, ...rest }: PrivateRouteProps) {
+function PublicRoute({ children, path, redirect, ...rest }: PublicRouteProps) {
   const accessToken = Auth.get();
 
   return (
     <Route path={path} {...rest}>
-      {accessToken ? children : <Redirect to={redirect} />}
+      {!accessToken ? children : <Redirect to={redirect} />}
     </Route>
   );
 }
 
-export default PrivateRoute;
+export default PublicRoute;

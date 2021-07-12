b82605461961c5287873f8a5cd69e46fae87250d
Leehaeun0 <a01050675111@gmail.com>
2021-07-12 23:15:15 +0900

    remove: theme 안쓰는 속성 제거

--- results/leehaeun0/src/styles/theme.ts
+++ results/leehaeun0/src/styles/theme.ts
@@ -2,13 +2,9 @@ const theme = {
   color: {
     white: '#FFFFFF',
     lightGray: '#F7F7F7',
-    softGray: '#F2F2F2',
-    line: '#DDDDDD',
     gray: '#B0B0B0',
-    darkGray: '#717171',
     main: '#396afc',
     error: '#FF1616',
-    softBlack: '#484848',
     black: '#222222',
   },
   boxShadow: 'rgb(100 100 111 / 15%) 7px 23px 62px',

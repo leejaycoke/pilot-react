2d903f12a7dbc9e062e448eda6aef7b3e6ee796d
yukyung <s2018w39@e-mirim.hs.kr>
2021-07-12 23:11:01 +0900

    refactor: util 함수들 class로 수정

--- results/cozy60/src/library/api/auth/auth.ts
+++ results/cozy60/src/library/api/auth/auth.ts
@@ -1,23 +1,26 @@
 import apiClient from '../apiClient';
 
-export const signIn = async (account: string, password: string) => {
-  const res = await apiClient.post('/auth/login', {
-    account: account,
-    password: password,
-  });
+export class AuthApi {
+  static async signIn(account: string, password: string) {
+    const res = await apiClient.post('/auth/login', {
+      account: account,
+      password: password,
+    });
+    window.sessionStorage.setItem('accessToken', res.data?.accessToken);
 
-  return res;
-};
+    return res;
+  }
 
-export const signOut = async () => {
-  const res = await apiClient.get('/auth/logout');
-  sessionStorage.clear();
+  static async signOut() {
+    const res = await apiClient.get('/auth/logout');
+    sessionStorage.clear();
 
-  return res;
-};
+    return res;
+  }
 
-export const getUserInfo = async () => {
-  const res = await apiClient.get('/v1/users/me');
+  static async getUserInfo() {
+    const res = await apiClient.get('/v1/users/me');
 
-  return res;
-};
+    return res;
+  }
+}

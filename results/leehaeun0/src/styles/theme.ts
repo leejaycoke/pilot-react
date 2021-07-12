56deb2707bd5314aa9c824eb7e277b9c145471e3
Leehaeun0 <a01050675111@gmail.com>
2021-07-12 12:37:09 +0900

    Add: theme 값 추가

--- results/leehaeun0/src/styles/theme.ts
+++ results/leehaeun0/src/styles/theme.ts
@@ -1,15 +1,17 @@
 const theme = {
   color: {
     white: '#FFFFFF',
+    lightGray: '#F7F7F7',
     softGray: '#F2F2F2',
     line: '#DDDDDD',
     gray: '#B0B0B0',
     darkGray: '#717171',
-    main: '#FF385C',
-    warning: '#D93900',
+    main: '#396afc',
+    error: '#FF1616',
     softBlack: '#484848',
     black: '#222222',
   },
+  boxShadow: 'rgb(100 100 111 / 15%) 7px 23px 62px',
 };
 
 export default theme;

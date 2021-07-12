cb39ac42568938679564ba81fb440cd79895497d
cozy60 <s2018w39@e-mirim.hs.kr>
2021-07-12 17:17:42 +0900

    feat: auth 관련 유틸 함수들 추가

---
+++ results/cozy60/src/library/api/auth/auth.ts
@@ -0,0 +1,22 @@
+import apiClient from '../apiClient';
+
+export const signIn = async (account: string, password: string) => {
+  const res = await apiClient.post('/auth/login', {
+    account: account,
+    password: password,
+  });
+
+  return res;
+};
+
+export const signOut = async () => {
+  const res = await apiClient.get('/auth/logout');
+
+  return res;
+};
+
+export const getUserInfo = async () => {
+  const res = await apiClient.get('/v1/users/me');
+
+  return res;
+};

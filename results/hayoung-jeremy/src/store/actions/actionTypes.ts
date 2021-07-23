68c3df832f293bca421c25f8ed6e31dcb7f493f9
Ha young, Kim <jeremiah91@naver.com>
2021-07-23 11:30:40 +0900

    feat : Login 구현 시도(redux-toolkit)
     - action type 변경 (createAction)
     - reducer 변경 (createReducer)

--- results/hayoung-jeremy/src/store/actions/actionTypes.ts
+++ results/hayoung-jeremy/src/store/actions/actionTypes.ts
@@ -1,21 +1,7 @@
-import { loginInfo } from "../../types/login";
+import { createAction } from "@reduxjs/toolkit";
 
-export const LOGIN_USER = "LOGIN_USER" as const;
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS" as const;
-export const LOGIN_FAIL = "LOGIN_FAIL" as const;
-export const SET_ERROR_MESSAGE = "SET_ERROR_MESSAGE" as const;
-export const CLEAR_ERROR_MESSAGE = "CLEAR_ERROR_MESSAGE" as const;
-
-export interface IloginAction {
-  type: typeof LOGIN_SUCCESS | typeof LOGIN_FAIL;
-  payload: loginInfo;
-}
-
-export interface IErrorMessageAction {
-  type: typeof SET_ERROR_MESSAGE | typeof CLEAR_ERROR_MESSAGE;
-}
-
-export interface loginAction {
-  type: typeof LOGIN_USER;
-  payload: loginInfo;
-}
+export const LOGIN_USER = createAction("LOGIN_USER");
+export const LOGIN_SUCCESS = createAction("LOGIN_SUCCESS");
+export const LOGIN_FAIL = createAction("LOGIN_FAIL");
+export const SET_ERROR_MESSAGE = createAction("SET_ERROR_MESSAGE");
+export const CLEAR_ERROR_MESSAGE = createAction("CLEAR_ERROR_MESSAGE");
--- results/hayoung-jeremy/src/store/reducers/auth.ts
+++ results/hayoung-jeremy/src/store/reducers/auth.ts
@@ -1,21 +1,22 @@
-import {
-  IloginAction,
-  LOGIN_FAIL,
-  LOGIN_SUCCESS,
-} from "../actions/actionTypes";
+import { createReducer } from "@reduxjs/toolkit";
+import { LOGIN_FAIL, LOGIN_SUCCESS } from "../actions/actionTypes";
 
 const initialState = {
   account: "",
   password: "",
 };
-
-export const loginReducer = (state = initialState, action: IloginAction) => {
-  switch (action.type) {
-    case LOGIN_SUCCESS:
-      return { ...state };
-    case LOGIN_FAIL:
-      return { ...state };
-    default:
-      return state;
-  }
-};
+//
+// export const loginReducer = (state = initialState, action: IloginAction) => {
+//   switch (action.type) {
+//     case LOGIN_SUCCESS:
+//       return { ...state };
+//     case LOGIN_FAIL:
+//       return { ...state };
+//     default:
+//       return state;
+//   }
+// };
+export const loginReducer = createReducer(initialState, {
+  [LOGIN_SUCCESS.type]: (state, action) => state + action.payload,
+  [LOGIN_FAIL.type]: (state, action) => state + action.payload,
+});

7d241b222899cd9d3e251e0f21f850bdc7f8bdd4
Ha young, Kim <jeremiah91@naver.com>
2021-07-30 10:49:14 +0900

    feat : Create axios Class and instance
     - axios 를 사용하여 api 요청 클래스 생성

--- results/hayoung-jeremy/src/service/api.ts
+++ results/hayoung-jeremy/src/service/api.ts
@@ -1,11 +1,41 @@
 import axios from "axios";
 
-const apiClient = axios.create({
-  baseURL: "http://localhost:5000",
-});
+// const apiClient = axios.create({
+//   baseURL: "http://localhost:5000",
+// });
+//
+// export const token = localStorage.getItem("userToken");
+//
+// apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+//
+// export default apiClient;
 
-const token = window.sessionStorage.getItem("userToken");
+class ApiClient {
+  private accessToken?: string | null;
+  private service: any;
 
-apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+  constructor(accessToken?: string | null) {
+    this.accessToken = accessToken;
+    let service = axios.create({
+      timeout: 1000,
+      baseURL: "http://localhost:5000",
+    });
+    service.defaults.headers["Authorization"] = `Bearer ${accessToken}`;
+    service.defaults.headers.post["Content-Type"] = "application/json";
+    this.service = service;
+  }
+  public get = (url: string, accessToken: string | unknown) => {
+    return this.service.get(url, accessToken);
+  };
+  public post = (url: string, payload: any) => {
+    return this.service.post(url, payload);
+  };
+}
+
+// export default ApiClient;
+
+const userToken = localStorage.getItem("userToken");
+
+const apiClient = new ApiClient(userToken);
 
 export default apiClient;
